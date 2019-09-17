cc0fcfc6e889cfa422a05c1524c8bf734b9f6991
KimMooHyeon <dlfb77@gmail.com>
2019-09-17 23:19:11 +0900

    add LoginActivity and Splach as Test

--- app/src/main/java/com/song2/jeonha/Login/LoginActivity.kt
+++ app/src/main/java/com/song2/jeonha/Login/LoginActivity.kt
@@ -1,14 +1,13 @@
-package com.song2.jeonha
+package com.song2.jeonha.Login
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import org.jetbrains.anko.startActivity
+import com.song2.jeonha.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.activity_login)
     }
 }
