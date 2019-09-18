f5dab5f405c9893e9821367330975d10fd1f0827
KimMooHyeon <dlfb77@gmail.com>
2019-09-18 21:50:41 +0900

    add QRcodeReader

--- app/src/main/java/com/song2/jeonha/Login/LoginActivity.kt
+++ app/src/main/java/com/song2/jeonha/Login/LoginActivity.kt
@@ -2,12 +2,19 @@ package com.song2.jeonha.Login
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.song2.jeonha.Main.MainActivity
 import com.song2.jeonha.R
+import kotlinx.android.synthetic.main.activity_login.*
+import org.jetbrains.anko.startActivity
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        tv_next.setOnClickListener {
+            startActivity<MainActivity>()
+            finish()
+        }
     }
 }
---
+++ app/src/main/java/com/song2/jeonha/Main/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.song2.jeonha.Main
+
+import android.app.Activity
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.google.zxing.integration.android.IntentIntegrator
+import com.google.zxing.integration.android.IntentResult
+import com.song2.jeonha.Main.QRcode.QRcodeActivity
+import com.song2.jeonha.R
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.startActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        iv_main_QRtest.setOnClickListener {
+            onQrcodeScanner()
+            startActivity<QRcodeActivity>()
+        }
+
+    }
+    private fun onQrcodeScanner() {
+
+        Toast.makeText(this, "Camera", Toast.LENGTH_SHORT).show()
+
+        val integrator = IntentIntegrator(this)
+        integrator.setBeepEnabled(false)
+        integrator.captureActivity = QRcodeActivity::class.java
+        integrator.initiateScan()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode == Activity.RESULT_OK) {
+            val scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+            val re = scanResult.contents
+            Toast.makeText(this, "$re", Toast.LENGTH_LONG).show()
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+
+}
