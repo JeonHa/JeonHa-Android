0e0bd54b1aba1fefa43e7cb3f1fc016253457468
choidaye <soar_dy@naver.com>
2019-09-20 15:26:07 +0900

    login activity ui

---
+++ app/src/main/java/com/song2/jeonha/Login/SignUpActivity.kt
@@ -0,0 +1,13 @@
+package com.song2.jeonha.Login
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.song2.jeonha.R
+
+class SignUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
