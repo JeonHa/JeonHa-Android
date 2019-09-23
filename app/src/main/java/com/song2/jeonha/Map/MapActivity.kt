954346a6847ad6e10ff68b745037a123d634976d
LeeSohee <shfksthgml@naver.com>
2019-09-23 22:23:13 +0900

    <MapActivity.kt> fusedLocationClient 로 현재위치 얻어오기

--- app/src/main/java/com/song2/jeonha/Map/MapActivity.kt
+++ app/src/main/java/com/song2/jeonha/Map/MapActivity.kt
@@ -9,19 +9,19 @@ import android.os.Looper
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
 import android.util.Log
-import android.widget.Toast
-import com.google.android.gms.common.api.GoogleApiClient
 import com.google.android.gms.location.*
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.song2.jeonha.R
 import org.jetbrains.anko.toast
 
-class MapActivity : AppCompatActivity(), OnMapReadyCallback, ActivityCompat.OnRequestPermissionsResultCallback {
+class MapActivity : AppCompatActivity(), OnMapReadyCallback,
+    ActivityCompat.OnRequestPermissionsResultCallback {
 
     val TAG: String = "MapActivity TAG"
 
@@ -30,18 +30,16 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback, ActivityCompat.OnRe
         Manifest.permission.ACCESS_FINE_LOCATION,
         Manifest.permission.ACCESS_COARSE_LOCATION
     )
-/*
 
     private val hasFineLocationPermission =
         ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
     private val hasCoarseLocationPermission =
         ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
 
-*/
 
-    private var fusedLocationClient: FusedLocationProviderApi? = null
+    private var fusedLocationClient: FusedLocationProviderClient? = null
     private var location: Location? = null
-    private var locationRequest : LocationRequest? = null
+    private var locationRequest: LocationRequest? = null
 
     private var currentLatLng: LatLng? = null
 
@@ -51,12 +49,12 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback, ActivityCompat.OnRe
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
 
-/*
+
         getPermission()
-        createLocationRequest()*/
+        createLocationRequest()
         addMap()
     }
-/*
+
     internal var locationCallback: LocationCallback = object : LocationCallback() {
         override fun onLocationResult(locationResult: LocationResult?) {
             super.onLocationResult(locationResult)
@@ -64,19 +62,34 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback, ActivityCompat.OnRe
             val locationList = locationResult!!.locations
 
             if (locationList.size > 0) {
+                mMap!!.clear()
+
                 location = locationList[locationList.size - 1]
 
                 currentLatLng = LatLng(location!!.latitude, location!!.longitude)
+
+                focusToPosition(currentLatLng!!)
             }
         }
-    }*/
+    }
 
-   /* private fun getPermission() {
+    private fun focusToPosition(currentLatLng: LatLng) {
+        val markerOptions = MarkerOptions()
+        markerOptions.position(currentLatLng)
+            .title("현재 위치")
+            .icon(BitmapDescriptorFactory.fromResource(R.drawable.mylocation_icon))
+
+        mMap!!.addMarker(markerOptions)
+        mMap!!.moveCamera(CameraUpdateFactory.newLatLng(currentLatLng))
+        mMap!!.animateCamera(CameraUpdateFactory.zoomTo(15f))
+    }
+
+    private fun getPermission() {
 
         if (hasFineLocationPermission == PackageManager.PERMISSION_GRANTED && hasCoarseLocationPermission == PackageManager.PERMISSION_GRANTED) {
-            *//**
+            /**
              * 권한을 허용했다면 위치를 요청합니다.
-             *//*
+             */
             requestLocation()
 
         } else {
@@ -101,25 +114,38 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback, ActivityCompat.OnRe
 
             }
         }
-    }*/
+    }
+
     private fun createLocationRequest() {
         locationRequest = LocationRequest()
             .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY) //create a location request
             .setInterval(60 * 1000) //이 메소드는 앱에서 위치 업데이트 수신간격을 밀리초단위로 설정한다.
     }
-//
-//    private fun requestLocation() {
-//        if (hasFineLocationPermission != PackageManager.PERMISSION_GRANTED && hasCoarseLocationPermission != PackageManager.PERMISSION_GRANTED) {
-//            /**
-//             * 권한 요청이 허가되어 있지 않은 경우
-//             * 권한 요청을 합니다.
-//             */
-//            getPermission()
-//        }
-//
-//        fusedLocationClient = GoogleApiClient
-//        fusedLocationClient.requestLocationUpdates(locationCallback,locationRequest,Looper.myLooper())
-//    }
+
+    private fun requestLocation() {
+        if (hasFineLocationPermission != PackageManager.PERMISSION_GRANTED && hasCoarseLocationPermission != PackageManager.PERMISSION_GRANTED) {
+            /**
+             * 권한 요청이 허가되어 있지 않은 경우
+             * 권한 요청을 합니다.
+             */
+            getPermission()
+        }
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        locationRequest = LocationRequest.create().let {
+            it.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+            it.setInterval(60 * 1000)
+        }
+
+        fusedLocationClient!!.requestLocationUpdates(
+            locationRequest,
+            locationCallback,
+            Looper.myLooper()
+        )
+
+
+    }
 
     private fun addMap() {
         Log.d(TAG, "addMap()")
@@ -152,5 +178,13 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback, ActivityCompat.OnRe
         }
 
 
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop()")
+
+        if (fusedLocationClient != null)
+            fusedLocationClient!!.removeLocationUpdates(locationCallback)
     }
 }
