54ef881deed5c94e4ac14f1081d59107af17b41e
seunghee63 <0603yang@naver.com>
2019-09-24 07:52:22 +0900

    Add main_ui

---
+++ app/src/main/java/com/song2/jeonha/Main/ProgramData.kt
@@ -0,0 +1,7 @@
+package com.song2.jeonha.Main
+
+data class ProgramListData(
+    var id : Int,
+    var program_img: String?,
+    var program_name: String?
+)
---
+++ app/src/main/java/com/song2/jeonha/Main/TitleData.kt
@@ -0,0 +1,7 @@
+package com.song2.jeonha.Main
+
+data class TitleData(
+    var id : Int,
+    var program_img: String?,
+    var program_name: String?
+)
