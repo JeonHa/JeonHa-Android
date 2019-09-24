e2a15828b193ab3efb93bd5f12139dae47050a59
LeeSohee <shfksthgml@naver.com>
2019-09-24 12:58:49 +0900

    <ClassListActivity.kt> layout

---
+++ app/src/main/java/com/song2/jeonha/Class/ClassListActivity.kt
@@ -0,0 +1,13 @@
+package com.song2.jeonha.Class
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.song2.jeonha.R
+
+class ClassListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_class_list)
+    }
+}
